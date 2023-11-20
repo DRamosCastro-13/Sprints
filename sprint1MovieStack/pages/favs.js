d4f0bfc87ad9654c90209c807c16d9d03debe7d3
Daniela Ramos <danirc01@gmail.com>
2023-11-20 10:06:12 -0500

    Ajustes fav y estilo

--- sprint1MovieStack/pages/favs.js
+++ sprint1MovieStack/pages/favs.js
@@ -1,4 +1,4 @@
-import{createCard, createTemplate, printTemplate} from '../module/funciones.js'
+import{createCard, printTemplate} from '../module/funciones.js'
 
 const url = `https://moviestack.onrender.com/api/movies`
 const apiKey = "0ff70d54-dc0b-4262-9c3d-776cb0f34dbd"
@@ -9,8 +9,6 @@ const options = {
 }
 let movies = []
 
-
-
 fetch(url, options)
     .then(response => response.json())
     .then(dataAPI => {
@@ -20,13 +18,12 @@ fetch(url, options)
 
         for(const movie of movies){
             if(favoriteMovies.includes(movie.id)){
-                favoritesPrint.push(movie)
-                
+                favoritesPrint.push(movie)   
             }
         }
 
         const containerFavs = document.getElementById('containerFavs');
-        printTemplate(favoritesPrint, containerFavs, createCard);
+       
         
 
         containerFavs.addEventListener('click', (e)=>{
@@ -35,15 +32,15 @@ fetch(url, options)
                 const botonFavId = e.target.dataset.id
                 if(favoriteMovies.includes(botonFavId)){
                     favoriteMovies.splice(favoriteMovies.indexOf(botonFavId), 1);
-                    e.target.setAttribute('fill' , 'rgba(0, 0, 0, 0.5)');
-                } else {
-                    favoriteMovies.push(botonFavId)
-                    e.target.setAttribute('fill', 'rgb(220 38 38)');   
-                }
+                } 
                 localStorage.setItem('favorites', JSON.stringify(favoriteMovies) )
+                
             }
-        })
+        }
+        )
         
+        printTemplate(favoritesPrint, containerFavs, createCard);
         
     })
     .catch(err => console.log(err));
+  
